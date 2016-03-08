cde3e8b427618e8a862bf20af295765b398cea4b
Avinash Verma <eyedagger@gmail.com>
2016-03-08 16:04:53 +0530

    added comments

--- select-and-tag.js
+++ select-and-tag.js
@@ -1,19 +1,62 @@
-(function () {
-	var ATTR_NAME = 'data-select-and-tag';
-	var elements = document.querySelectorAll('[' + ATTR_NAME + ']');
+'use strict';
 
-	for(var i = 0, iL = elements.length; i < iL; i++) {
-		var element = elements[i];
+/**
+ * @author Avinash Verma < avinashverma.it@gmail.com >
+ *
+ * This simple utility lets you retrieve the selected text from an HTML element.
+ *
+ * @example <div select-and-tag>Some content<div>
+ */
+
+
+function SelectAndTag (options) {
+	var ATTR_NAME = 'select-and-tag';
+
+	var elements,
+		element,
+		selection,
+		text,
+		start,
+		end,
+		selectedText,
+		i,
+		iL,
+		options;
+
+	options = options || {};
+	options.onSelect = options.onSelect || function (selectedText) {
+		console.log(selectedText);
+	}
+	//Get all elements with attribute name 'select-and-tag'
+	elements = document.querySelectorAll('[' + ATTR_NAME + ']');
+
+	//For each element, add a onmouseup event
+	for ( i = 0, iL = elements.length; i < iL; i++) {
+		element = elements[i];
+
 		element.onmouseup = function (event) {
-			var selection = window.getSelection();
-			var text = selection.anchorNode.data;
-			var start = selection.anchorOffset;
-			var end = selection.focusOffset;
+			//Get the selection Object
+			selection = window.getSelection();
 
-			if(start !== end) {
-				var selectedText = text.substring(start, end);
+			//Get the complete text
+			text = selection.anchorNode.data;
+
+			//Get the start index
+			start = selection.anchorOffset;
+
+			//Get the end index
+			end = selection.focusOffset;
+
+			//Get the Selected Text
+			selectedText = text.substring(start, end).trim();
+
+			//Check if selection is invalid or if it contains just blank spaces
+			if (start !== end && selectedText.length > 0) {
+
+				//Set the attribute value
 				element.setAttribute(ATTR_NAME, selectedText);
+				options.onSelect.call(null, selectedText);
 			}
 		}
 	}
-})()
+}
